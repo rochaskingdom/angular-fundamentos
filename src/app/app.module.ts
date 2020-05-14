a912f98e053e3d40e94fc1c01bc5b09c5069922e
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2020-05-14 17:33:39 -0300

    Binding de propriedades customizadas com @Input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
+import { FuncionarioCardComponent } from './funcionario-card/funcionario-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FuncionarioCardComponent
   ],
     imports: [
         BrowserModule,
