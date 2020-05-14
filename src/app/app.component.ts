865d494c0146ee88fae2204d2ed082eb5f58499b
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2020-05-14 13:10:51 -0300

    Usando interpolação

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'meu-projeto';
+  nome = 'Vinicius';
+  idade = 10;
+
+  getIdade() {
+    return this.idade;
+  }
 }
