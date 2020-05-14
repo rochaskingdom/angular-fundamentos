eea315844e576c646a591438988f58fa5e030d28
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2020-05-14 17:10:13 -0300

    Iterando com a diretiva *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,19 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  ultimoId = 0;
   nome = 'Vinicius';
   adicionado = false;
+  funcionarios = [];
 
   adicionar() {
     console.log(`Adicionando ${this.nome}`);
     this.adicionado = true;
+
+    this.funcionarios.push({
+      id: this.ultimoId++,
+      nome: this.nome
+    });
   }
 
 }
