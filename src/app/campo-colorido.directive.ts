f6deb835c8d882df10cfc2eff54c9d952dcfac04
Vinicius Rodrigues Rocha <viniciusr.rocha@outlook.com>
2020-05-15 02:00:34 -0300

    Vinculando propriedades do hospedeiro com @HostBinding

--- src/app/campo-colorido.directive.ts
+++ src/app/campo-colorido.directive.ts
@@ -1,24 +1,18 @@
-import {Directive, ElementRef, HostListener, Renderer2} from '@angular/core';
+import {Directive, HostBinding, HostListener} from '@angular/core';
 
 @Directive({
   selector: '[appCampoColorido]'
 })
 export class CampoColoridoDirective {
 
-  constructor(
-    private elementRef: ElementRef,
-    private renderer: Renderer2
-  ) {
-  }
+  @HostBinding('style.backgroundColor') corDeFundo: string;
 
   @HostListener('focus') onFocus() {
-    this.renderer.setStyle(this.elementRef.nativeElement,
-      'background-color', 'yellow')
+    this.corDeFundo = 'coral';
   }
 
   @HostListener('blur') offFocus() {
-    this.renderer.setStyle(this.elementRef.nativeElement,
-      'background-color', 'transparent')
+    this.corDeFundo = 'transparent'
   }
 
 }
